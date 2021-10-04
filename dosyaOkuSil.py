2be96970e2ee82674c4a85791b67fdbc50778a4c
recepilyasoglu <rcp.ilyasoglu@gmail.com>
2021-10-04 18:28:15 +0300

    Python Read/Delete File

---
+++ dosyaOkuSil.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+data1 = pd.read_excel("dwa_police_report_20180924.xlsx")
+
+print(data1)
+
+import sqlite3
+
+def dosyayıOku():
+    connection = sqlite3.connect("ddl2.db")
+    
+    cursor = connection.execute("Select * From recep2")
+    data = cursor.fetchall()
+    print (data)
+
+
+dosyayıOku()
+
+# import sqlite3
+
+
+# connection = sqlite3.connect("ddl.db")
+# cursor = connection.execute("DELETE FROM recep")
+# connection.commit()
+# connection.close()
+
+
+    
+
+
+
+
